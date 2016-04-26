ba1b634f0caee0edec08bdf06bd570450bb40240
Noodlewood <daniel.riedmuller@gmail.com>
2016-04-26 08:53:04 +0200

    init

---
+++ BueroPactBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Buero\PactBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BueroPactBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Buero\PactBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BueroPactBundle:Default:index.html.twig');
+    }
+}
